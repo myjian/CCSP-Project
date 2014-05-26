3a9a103d810173e42338a52d9d0e65737931bf43
Ming-Yuan Jian <b99902067@csie.ntu.edu.tw>
2014-05-26 21:24:21 +0800

    merge javascripts on reportBadDriver.html

--- public/js/baddriver.js
+++ public/js/baddriver.js
@@ -1,18 +1,23 @@
-function validateForm(){
-    if (document.getElementById("plate").value === "")
+function isValid(id){
+    var item = document.getElementById(id);
+    if (item.value === ""){
+        alert(item.placeholder + "不可空白");
+        item.focus();
         return false;
-
-    if (document.getElementById("country").value === "")
-        return false;
-
-    if (document.getElementById("road").value === "")
-        return false;
-
-    if (document.getElementById("condition").value === "")
-        return false;
-
-    if (document.getElementById("url").value === "")
-        return false;
-
+    }
     return true;
+}
+
+function validateForm(){
+    if (!isValid('plate')) return false;
+    if (!isValid("country")) return false;
+    if (!isValid("road")) return false;
+    if (!isValid("condition")) return false;
+    if (!isValid("url")) return false;
+    return true;
+}
+
+function fillTable(){
+    var iframe = $('<iframe id="dataframe" name="dataframe" src="//www.tcpd.gov.tw/tcpd/cht/index.php?act=traffic&amp;code=add" frameborder="0" marginheight="0" marginwidth="0" width="100%" height="1100" scrolling="auto"></iframe>');
+    $("#frame").append(iframe);
 }
