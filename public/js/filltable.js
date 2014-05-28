9cd2feb38d602da1e1bc2004fa44551ad2423441
MitchTseng <zxc1020305@gmail.com>
2014-05-28 18:25:09 +0800

    更改現在就去檢舉的位置

--- public/js/filltable.js
+++ public/js/filltable.js
@@ -1,5 +1,5 @@
-var info = {taipei: '<img style="width:100%;" src="/fig/台北市.jpg"><a class="btn btn-default col-xs-10 col-xs-offset-1" href="https://www.tcpd.gov.tw/tcpd/cht/index.php?act=traffic&code=add">現在就去檢舉</a>',
-highway: '<img style="width:100%;" src="/fig/國道.jpg"><a class="btn btn-default col-xs-10 col-xs-offset-1" href="https://services.hpb.gov.tw/WOSWeb/RVDefault.aspx">現在就去檢舉</a>'};
+var info = {taipei: '<a class="btn btn-default col-xs-10 col-xs-offset-1" href="https://www.tcpd.gov.tw/tcpd/cht/index.php?act=traffic&code=add">現在就去檢舉</a><img style="width:100%;" src="/fig/台北市.jpg">',
+highway: '<a class="btn btn-default col-xs-10 col-xs-offset-1" href="https://services.hpb.gov.tw/WOSWeb/RVDefault.aspx">現在就去檢舉</a><img style="width:100%;" src="/fig/國道.jpg">'};
 
 $("#country").on('change',function(){
 
