5a7cb5dc6a0b779aab352f1083ef37f0ecc081ac
MitchTseng <zxc1020305@gmail.com>
2014-05-28 22:29:44 +0800

    加入聯絡我們

--- public/js/filltable.js
+++ public/js/filltable.js
@@ -1,5 +1,5 @@
-var info = {taipei: '<a class="btn btn-default col-xs-10 col-xs-offset-1" href="https://www.tcpd.gov.tw/tcpd/cht/index.php?act=traffic&code=add">現在就去檢舉</a><img style="width:100%;" src="/fig/台北市.jpg">',
-highway: '<a class="btn btn-default col-xs-10 col-xs-offset-1" href="https://services.hpb.gov.tw/WOSWeb/RVDefault.aspx">現在就去檢舉</a><img style="width:100%;" src="/fig/國道.jpg">'};
+var info = {taipei: '<br><a class="btn btn-danger col-xs-10 col-xs-offset-1" href="https://www.tcpd.gov.tw/tcpd/cht/index.php?act=traffic&code=add">現在就去檢舉</a><br><br><br><img class="img" style="width:100%;" src="/fig/台北市.jpg">',
+highway: '<br><a class="btn btn-danger col-xs-10 col-xs-offset-1" href="https://services.hpb.gov.tw/WOSWeb/RVDefault.aspx">現在就去檢舉</a><br><br><br><img class="img" style="width:100%;" src="/fig/國道.jpg">'};
 
 $("#country").on('change',function(){
 
@@ -11,12 +11,27 @@ $("#country").on('change',function(){
 	if(this.options[this.selectedIndex].value == "台北市")
 	{
 		$("#reportInfo").hide();
-		$("#information").append(info.taipei);
+
+		if(screen.width > 800)
+		{			
+			$("#information").append('<div style="width:80%; margin-left:10%; margin-right:10%;">'+info.taipei+'</div>');
+		}
+		else
+		{
+			$("#information").append(info.taipei);	
+		}
 	}
 	else if (this.options[this.selectedIndex].value == "國道") 
 	{
 		$("#reportInfo").hide();
-		$("#information").append(info.highway);
+		if(screen.width > 800)
+		{			
+			$("#information").append('<div style="width:80%; margin-left:10%; margin-right:10%;">'+info.highway+'</div>');
+		}
+		else
+		{
+			$("#information").append(info.highway);
+		}
 	}
 	else
 	{
