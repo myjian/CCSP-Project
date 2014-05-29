d4c61640ae9b1eccf86fe5c8b9cc1fc591226d45
MitchTseng <zxc1020305@gmail.com>
2014-05-29 11:06:37 +0800

    QA收起彈出

---
+++ public/js/filltips.js
@@ -0,0 +1,34 @@
+$("#Q1").on('click',function(){
+	$(".answer").hide();
+	$("#A1").show();
+});
+
+$("#Q2").on('click',function(){
+	$(".answer").hide();
+	$("#A2").show();
+});
+
+$("#Q3").on('click',function(){
+	$(".answer").hide();
+	$("#A3").show();
+});
+
+$("#Q4").on('click',function(){
+	$(".answer").hide();
+	$("#A4").show();
+});
+
+$("#Q5").on('click',function(){
+	$(".answer").hide();
+	$("#A5").show();
+});
+
+$("#Q6").on('click',function(){
+	$(".answer").hide();
+	$("#A6").show();
+});
+
+$("#Q7").on('click',function(){
+	$(".answer").hide();
+	$("#A7").show();
+});
