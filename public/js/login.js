eb880fdb70908e0ab58d76327906b02ec8514dad
MitchTseng <zxc1020305@gmail.com>
2014-06-03 23:43:10 +0800

    拿掉案件列表的影片/照片按鈕與右上角的資料

--- public/js/login.js
+++ public/js/login.js
@@ -1,5 +1,5 @@
 var fbLogin = '<a id="fblogin" class="btn btn-primary" href="/auth/facebook">FB<br>登入</a>';
-var fbLogout = '<a href="/userinfo"><button style="background-color: #5b74a8; color: #FFFFFF">資料</button></a><a href="/logout"><button style="background-color: #5b74a8; color: #FFFFFF">登出</button></a>';
+var fbLogout = '<a href="/logout"><button style="background-color: #5b74a8; color: #FFFFFF">登出</button></a>';
 
 function displayFB(user){
     var fbArea = $("#fbArea");
