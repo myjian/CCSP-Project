e5067ac519623d56608b2edbadbf13dc13e7f797
Ming-yuan <1978764+myjian@users.noreply.github.com>
2023-06-20 05:24:33 +0900

    Rename local database "baddriver"

--- db.js
+++ db.js
@@ -1,28 +1,29 @@
 var mongoose = require('mongoose');
 
 exports.init = function(app) {
-    var mongoURI = process.env.MONGOLAB_URI || 'mongodb://localhost:27017/test';
+  var mongoURI =
+    process.env.MONGOLAB_URI || 'mongodb://localhost:27017/baddriver';
 
-    // Prevent false-positive deprecation message
-    // See https://github.com/Automattic/mongoose/issues/4291 for more details
-    mongoose.Promise = global.Promise;
+  // Prevent false-positive deprecation message
+  // See https://github.com/Automattic/mongoose/issues/4291 for more details
+  mongoose.Promise = global.Promise;
 
-    mongoose.connect(mongoURI, {useNewUrlParser: true, useUnifiedTopology: true});
+  mongoose.connect(mongoURI, {useNewUrlParser: true, useUnifiedTopology: true});
 
-    var conn = mongoose.connection;
-    // Error handler
-    conn.on('error', function(err){
-        console.error(err);
-    });
+  var conn = mongoose.connection;
+  // Error handler
+  conn.on('error', function(err){
+    console.error(err);
+  });
 
-    // Connection established
-    conn.once('open', function(){
-        const gridfs = new mongoose.mongo.GridFSBucket(conn.db);
-        app.set('gridfs', gridfs);
-        console.info('MongoDB connection established at ' + mongoURI);
-    });
+  // Connection established
+  conn.once('open', function(){
+    const gridfs = new mongoose.mongo.GridFSBucket(conn.db);
+    app.set('gridfs', gridfs);
+    console.info('MongoDB connection established at ' + mongoURI);
+  });
 
-    // Require schema
-    require('./models/driverRecord');
-    require('./models/userInfo');
+  // Require schema
+  require('./models/driverRecord');
+  require('./models/userInfo');
 };
